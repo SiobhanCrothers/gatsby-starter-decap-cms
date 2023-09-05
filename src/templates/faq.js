ba5ff48581be9308feff0f521936715194144ef3
Siobhan Crothers <siobhan.crothers@bravurasecurity.com>
2023-09-05 10:22:08 -0600

    Attempt to add accordion

--- src/templates/faq.js
+++ src/templates/faq.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
+import Accordion from "../components/Accordion"; 
 import Content, { HTMLContent } from "../components/Content";
 
 // eslint-disable-next-line
