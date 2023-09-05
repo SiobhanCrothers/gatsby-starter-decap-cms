e480406827a6179401426c51b85c4dc187ccce1e
SiobhanCrothers <143827054+SiobhanCrothers@users.noreply.github.com>
2023-09-05 14:26:13 -0600

    Putting back working FAQ page

---
+++ src/pages/faq/index.js
@@ -0,0 +1,45 @@
+import React, { useState, useRef, useEffect } from "react";
+import Layout from "../../components/Layout";
+import "./accordion-styles.css";
+
+export default function App() {
+  const [active, setActive] = useState(false);
+
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    contentRef.current.style.maxHeight = active
+      ? `${contentRef.current.scrollHeight}px`
+      : "0px";
+  }, [contentRef, active]);
+
+  const toggleAccordion = () => {
+    setActive(!active);
+  };
+  return (
+    <Layout>
+      <div className="App">
+        <div>
+          <button
+            className={`question-section ${active}`}
+            onClick={toggleAccordion}
+          >
+            <div>
+              <div className="question-align">
+                <h4 className="question-style">
+                  Why do you like web development
+                </h4>
+              </div>
+              <div
+                ref={contentRef}
+                className={active ? `answer answer-divider` : `answer`}
+              >
+                <p>Because I love coding</p>
+              </div>
+            </div>
+          </button>
+        </div>
+      </div>
+    </Layout>
+  );
+}
