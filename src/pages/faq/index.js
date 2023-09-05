fbf075b1dedeadb572a44d364174db4af78e7559
Siobhan Crothers <siobhan.crothers@bravurasecurity.com>
2023-09-05 10:54:42 -0600

    Add component

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
+import Layout from "../../components/Layout";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
