c3cbd8bded47a9b670bc894a385e4fbd2470100e
Siobhan Crothers <siobhan.crothers@bravurasecurity.com>
2023-09-06 17:28:10 -0600

    Fixing bracket error

--- src/cms/preview-templates/FaqQuestionPreview.js
+++ src/cms/preview-templates/FaqQuestionPreview.js
@@ -2,12 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { FaqQuestionTemplate } from '../../templates/faq-question'
 
-const FaqQuestionPreview = ({ entry, widgetFor }) => {
+const FaqQuestionPreview = ({ entry, widgetFor }) => (
 
   <FaqQuestionTemplate
     title={entry.getIn(['data', 'title'])}
     content={widgetFor('body')}
   />
+)
 
 FaqQuestionPreview.propTypes = {
   entry: PropTypes.shape({
