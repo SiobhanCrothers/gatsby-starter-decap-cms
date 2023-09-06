a480fc989fec133cbaebda239f6187123fc5d904
Siobhan Crothers <siobhan.crothers@bravurasecurity.com>
2023-09-06 16:11:26 -0600

    Adding logs

--- src/cms/preview-templates/FaqQuestionPreview.js
+++ src/cms/preview-templates/FaqQuestionPreview.js
@@ -3,10 +3,25 @@ import PropTypes from 'prop-types'
 import FaqQuestionTemplate from '../../templates/faq-question'
 
 const FaqQuestionPreview = ({ entry, widgetFor }) => {
-  const tags = entry.getIn(['data', 'tags'])
+  // Check if entry is defined
+  if (!entry) {
+    return <div>No preview available</div>;
+  }
+
+  // Check if data and title are defined
+  const data = entry.get('data');
+  const title = data ? data.get('title') : null;
+
+  // Check if title is defined
+  if (!title) {
+    return <div>No title available</div>;
+  }
+
+  const tags = data ? data.get('tags') : null;
+
   return (
     <FaqQuestionTemplate
-      title={entry.getIn(['data', 'title'])}
+      title={title}
       content={widgetFor('body')}
       tags={tags && tags.toJS()}
     />
